10f8a28d4c0912c34cc8d3cb1b7df9138e126b08
caokaifa <997068510@qq.com>
2021-11-28 14:09:58 +0800

    Add files via upload

---
+++ HiDO-MPC-ResQbot/README.md
@@ -0,0 +1,17 @@
+# HiDO-MPC-ResQbot
+Implementation of Hierarchical Decomposed Objective Model Predictive Control (HiDO-MPC) for Autonomous Casualty Approach Task.
+
+This repository is a complimentary material for the manuscript:
+
+  **Hierarchical Decomposed-ObjectiveModel Predictive Control forAutonomous Casualty Extraction**
+  
+This repository is still under construction to complete all the implementation code.
+
+## Prerequisite
+- CasADi link [here](https://web.casadi.org/)
+
+## Project website
+- Link [here](https://sites.google.com/view/hido-mpc-resqbot)
+
+## Cite the project
+R. P. Saputra, N. Rakicevic, D. Chappell, K. Wang and P. Kormushev, "Hierarchical Decomposed-Objective Model Predictive Control for Autonomous Casualty Extraction," in IEEE Access, doi: 10.1109/ACCESS.2021.3063782.
---
+++ HiDO-MPC-ResQbot/main.py
@@ -0,0 +1,46 @@
+
+""" 
+HiDO-MPC ResQbot Main Executable File 
+author: R Saputra
+2021
+"""
+
+import time
+import numpy as np
+import sys
+import os
+sys.path.append(os.path.abspath('/home/robert/catkin_ws/src/HiDO-MPC-ResQbot/corridor_experiments'))
+from corridor_experiments import HiDO_MPC_resqbot_main as HIDO
+from corridor_experiments.visualisation import simulate_robot
+
+METHOD = ['HIDO', 'SMPC', 'BMPC', 'VMPC']
+
+EXP_SETUP ={'1':[np.array([-10, -10, np.deg2rad(0)]),np.array([3.2, 0.0, 3.7, 0.0])], 
+            '2':[np.array([0.5, -0.5, np.deg2rad(0)]),np.array([3.2, 0.0, 3.7, 0.0])],
+            '3':[np.array([0.5, 0.5, np.deg2rad(0)]),np.array([3.2, 0.0, 3.7, 0.0])],
+            '4':[np.array([0.0, 0.0, np.deg2rad(0)]),np.array([3.2, -0.4, 3.7, -0.5])],
+            '5':[np.array([0.5, -0.5, np.deg2rad(0)]),np.array([3.2, -0.4, 3.7, -0.5])],
+            '6':[np.array([0.5, 0.5, np.deg2rad(0)]),np.array([3.2, -0.4, 3.7, -0.5])],
+            '7':[np.array([0.0, 0.0, np.deg2rad(0)]),np.array([3.2, 0.4, 3.7, 0.5])],
+            '8':[np.array([0.5, -0.5, np.deg2rad(0)]),np.array([3.2, 0.4, 3.7, 0.5])],
+            '9':[np.array([0.5, 0.5, np.deg2rad(0)]),np.array([3.2, 0.4, 3.7, 0.5])]}
+
+def set_init_goal(S):
+    V0 = EXP_SETUP[S][0]
+    C0 = EXP_SETUP[S][1]
+    return V0,C0
+
+if __name__ == '__main__':
+    T = .25      # sampling time [s]
+    N = 25       # prediction horizon
+    M_list = ['HIDO', 'SMPC', 'BMPC', 'VMPC']
+    S_list = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+    print('Start MPC!!!')
+    time.sleep(1)
+    for M in M_list:
+        for S in S_list:
+            V0,C0 = set_init_goal(S)
+            print("s88=",S)
+            t,xx,xx1,u_cl,xs,c1,dist2point_his, dist2line_his, angle_diff_his, mpc_proc_time_his = HIDO.main(V0, C0, T, N)
+            # print("5777777777777776=",u_cl)
+            simulate_robot(t, xx, xx1, u_cl, xs, N, T,c1, M, S)
